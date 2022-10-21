d0d34225bd5e77db979fd1aaf9f7f16cc6c82aee
Vlad Chubar <johndoe@example.com>
2022-10-21 15:25:42 +0300

    completed mainInfo

--- src/components/MainInfo/MainInfo.jsx
+++ src/components/MainInfo/MainInfo.jsx
@@ -1,20 +1,16 @@
 const MainInfo = ({ isMonthly, setButton }) => {
-  const monthly = isMonthly
-    ? 'mainInfo-container__btns__btn mainInfo-container__btns__btn_active'
-    : 'mainInfo-container__btns__btn';
-  const yearly = isMonthly
-    ? 'mainInfo-container__btns__btn '
-    : 'mainInfo-container__btns__btn mainInfo-container__btns__btn_active';
+  const monthly = isMonthly ? 'mainInfo__btn mainInfo__btn_active' : 'mainInfo__btn';
+  const yearly = isMonthly ? 'mainInfo__btn ' : 'mainInfo__btn mainInfo__btn_active';
 
   return (
     <div className="mainInfo">
       <h1 className="mainInfo__title">Plans & Pricing</h1>
-      <div className="mainInfo-container">
-        <p className="mainInfo-container__paragraph">
+      <div className="mainInfo__container">
+        <p className="mainInfo__paragraph">
           Whether your time-saving automation needs are large or small, we’re here to help you
           scale.
         </p>
-        <div className="mainInfo-container__btns">
+        <div className="mainInfo__btns-container">
           <button className={monthly} onClick={() => setButton(true)}>
             Monthly
           </button>
