f9c93e3dfc8f8a0dbf42537539357efe5eb2cfff
Vlad Chubar <johndoe@example.com>
2022-10-21 17:21:00 +0300

    premium card fix

--- src/components/Cards/Premium.jsx
+++ src/components/Cards/Premium.jsx
@@ -1,34 +1,34 @@
 const Premium = ({ isMonthly }) => {
   return (
-    <div className="main-premium-card">
-      <div className="most-popular">MOST POPULAR</div>
-      <div className="premium-card price-card">
+    <div className="main-premium">
+      <div className="main-premium__most-popular">MOST POPULAR</div>
+      <div className="price-card main-premium__card">
         <div className="price-card__price-container">
-          <h1 className="price-card__price white-text">{isMonthly ? '$89' : '$56'}</h1>
-          <span className="price-card__perMonth white-text">/month</span>
+          <h1 className="price-card__price main-premium__text">{isMonthly ? '$89' : '$56'}</h1>
+          <span className="price-card__perMonth main-premium__text">/month</span>
         </div>
         <div className="price-card__title-container">
-          <h2 className="price-card__title white-text">Company</h2>
+          <h2 className="price-card__title main-premium__text">Company</h2>
           <span className="price-card__info ">Automation plus enterprise-grade features.</span>
         </div>
-        <ul className="price-card__list premium-list">
+        <ul className="price-card__list main-premium__list">
           <li className="price-card__list-item ">
-            <i className="price-card__icon list-icon-pr"></i>Multi-step Zaps
+            <i className="price-card__icon main-premium__icon"></i>Multi-step Zaps
           </li>
           <li className="price-card__list-item ">
-            <i className="price-card__icon list-icon-pr"></i>Unlimited Premium Apps
+            <i className="price-card__icon main-premium__icon"></i>Unlimited Premium Apps
           </li>
           <li className="price-card__list-item ">
-            <i className="price-card__icon list-icon-pr"></i>Unlimited Users Team
+            <i className="price-card__icon main-premium__icon"></i>Unlimited Users Team
           </li>
           <li className="price-card__list-item ">
-            <i className="price-card__icon list-icon-pr"></i>Advanced Admin
+            <i className="price-card__icon main-premium__icon"></i>Advanced Admin
           </li>
           <li className="price-card__list-item ">
-            <i className="price-card__icon list-icon-pr"></i>Custom Data Retention
+            <i className="price-card__icon main-premium__icon"></i>Custom Data Retention
           </li>
         </ul>
-        <button className="price-card__btn premium-btn">Choose plan</button>
+        <button className="price-card__btn main-premium__btn">Choose plan</button>
       </div>
     </div>
   );
